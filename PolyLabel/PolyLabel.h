2b59c1796f3861ead7fcfc51044157c22fdc12e5
Nicolas Marchal <marchalnicolas@outlook.com>
2024-06-25 15:38:44 +0200

    Initial Commit

---
+++ PolyLabel/PolyLabel.h
@@ -0,0 +1,18 @@
+//
+//  PolyLabel.h
+//  PolyLabel
+//
+//  Created by Nicolas Marchal on 25/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PolyLabel.
+FOUNDATION_EXPORT double PolyLabelVersionNumber;
+
+//! Project version string for PolyLabel.
+FOUNDATION_EXPORT const unsigned char PolyLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PolyLabel/PublicHeader.h>
+
+
